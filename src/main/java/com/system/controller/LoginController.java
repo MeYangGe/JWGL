78eb4750ec38cb06d87c376f02951ee7261c0a1d
MeYangGe <63237524@qq.com>
2020-06-07 19:58:04 +0800

    把Login放到了LoginController

--- src/main/java/com/system/controller/LoginController.java
+++ src/main/java/com/system/controller/LoginController.java
@@ -1,28 +1,21 @@
 package com.system.controller;
 
-
 import com.system.model.User;
-import com.system.service.CourseService;
 import com.system.util.ResultVM;
-import com.github.pagehelper.PageInfo;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author  JGW
- * @date 2020-06-05 07:31
+ * @author JGW
  * @version 1.0
- * 课程Controller
+ * @date 2020/6/7 19:57
  */
 @RestController
-public class CourseController {
+public class LoginController {
     //登陆操作
     @GetMapping("/login")
     public ResultVM login(User user){
